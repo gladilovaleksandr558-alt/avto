539bce524238f4fdac9224c52e6d5a6c520231bd
gladilovaleksandr558-alt <gladilovaleksandr558@gmail.com>
2025-11-01 15:27:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("8041824382:AAEQRFNdN-nfaX7e6PhBoHs1FkQ13gVBCrw")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # ✅ имя переменной — BOT_TOKEN
 CHECK_INTERVAL = 300  # интервал в секундах
