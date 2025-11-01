df978aae58cd81c834410293df553bbdb66d5248
gladilovaleksandr558-alt <gladilovaleksandr558@gmail.com>
2025-11-01 18:38:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -208,7 +208,4 @@ if __name__ == "__main__":
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main())
     except Exception as e:
-        logging.error(f"❌ Ошибка запуска бота: {e}")
-
-        loop.run_until_complete(main())
-    except
+      
